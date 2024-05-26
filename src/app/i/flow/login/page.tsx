c09ca39c101f33ac96832e573b474d1dd8f99fe8
BIROOM <zoon1031@naver.com>
2024-05-26 20:35:40 +0900

    Initial commit from Create Next App

---
+++ src/app/i/flow/login/page.tsx
@@ -0,0 +1,10 @@
+import {redirect} from "next/navigation"
+
+
+export default function Login() {
+  return (
+    <div>
+        라우트 된 login 페이지
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import {
+	useRouter,
+	usePathname,
+	useSearchParams,
+	useSelectedLayoutSegment,
+	useSelectedLayoutSegments,
+	redirect,
+	notFound
+} from 'next/navigation';
+export default function Home() {
+
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
+    </>
+  
+  );
+}
