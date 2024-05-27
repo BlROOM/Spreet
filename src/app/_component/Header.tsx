7a1be44e01f4f50d587bd02ba7ac7f11e72a67a4
junyoungNA <nanasooo@naver.com>
2024-05-27 19:51:35 +0900

    loginModal 인터셉팅 라우트 오류 해결, header,LoginModal component

---
+++ src/app/_component/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-blue-800 text-white py-4">
+      <div className="container mx-auto px-4 flex align-middle justify-between">
+        <nav>
+          <h1 className="text-2xl font-bold">Spreet</h1>
+          <p className="text-sm">Spreed Street culture</p>
+        </nav>
+        <nav>
+          <ul className="flex gap-3">
+            <li>
+              <Link href="./search">행사</Link>
+            </li>
+            <li>
+              <Link href="./">강의</Link>
+            </li>
+            <li>
+              <Link href="./login">로그인</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
-'use client'
-import { useRouter } from "next/navigation"
+"use client";
+import { useRouter, redirect } from "next/navigation";
 
 export default function Login() {
-    const router = useRouter(); // client에서 리다이렉트
-    router.replace('/i/flow/login');
+  const router = useRouter(); // client에서 리다이렉트
+  router.replace("/i/flow/login");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      메인 페이지입니다.
+    </main>
+  );
 }
