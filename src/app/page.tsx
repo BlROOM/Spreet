f76a5194332d86064dd89502511df399affa16a4
BIROOM <zoon1031@naver.com>
2024-05-27 23:53:15 +0900

    handleClickSlide 함수, mainPage 캐러셀 상태 값 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+
 export default function Home() {
+  const spreetRef = useRef<HTMLDivElement>(null);
+  const [caroselX, setCaroselX] = useState(0);
+  useEffect(() => {
+    const getSpreetCordinate = () => {
+      if (!spreetRef.current) return;
+      const listLeft = spreetRef.current.getBoundingClientRect().left;
+      setCaroselX(listLeft);
+    };
+
+    getSpreetCordinate();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       메인 페이지입니다.
