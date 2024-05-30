43a21af6d28d612b737e80adcc6f0a17abe4aaea
BIROOM <zoon1031@naver.com>
2024-05-31 00:32:33 +0900

    MAIN 캐러셀 적용 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,9 @@ export default function Home() {
     "/images/main4.jpg",
   ];
   return (
-    <main className="flex flex-col items-center justify-between p-24 border">
-      메인 페이지입니다.
-      <div className="h-[300px] w-3/4 relative border border-black box-border">
-        <div className="w-full h-full relative overflow-hidden rounded-lg">
+    <main className="m-auto relative w-[1440px] h-[600px]">
+      <div className="h-full w-full relative border border-black box-border">
+        <div className=" h-full relative overflow-hidden">
           <Image
             src="/images/blackLeftLarge.png"
             className="cursor-pointer bg-inherit w-15 h-25 text-7xl text-white absolute flex justify-center items-center z-50 top-1/2 transform -translate-y-1/2 "
@@ -46,7 +45,7 @@ export default function Home() {
           />
           <Image
             src="/images/blackRightSmall.png"
-            className="cursor-pointer bg-inherit w-15 h-25 text-[100px] text-white absolute flex justify-center items-center z-99 top-1/2 transform -translate-y-1/2 right-0 m-0 pointer-events-auto"
+            className="cursor-pointer bg-inherit w-15 h-25 text-[100px] text-white absolute flex justify-center items-center z-50 top-1/2 transform -translate-y-1/2 right-0 m-0 pointer-events-auto"
             alt="rightArrow"
             width={50}
             height={30}
@@ -64,25 +63,19 @@ export default function Home() {
             className="relative w-full h-full flex items-center transform translate-custom transition-transform duration-500 ease-in-out"
             ref={spreetRef}
           >
-            <div className="flex h-3/5 items-center">
+            <div className="flex w-screen h-full items-center">
               {carouselDataList.map((item, idx) => {
                 return (
-                  <div key={idx} className="relative w-full h-full ml-32">
-                    <div
-                      key={idx}
-                      className="absolute w-full h-full z-1 opacity-10 bg-gray-700 rounded-2xl pointer-events-none"
-                    ></div>
-                    <div className="slide-item__shorts-title">
-                      <div className="user-profile">
-                        <div className="user-image">
-                          <Image
-                            src={item}
-                            width={100}
-                            height={100}
-                            alt="main_image"
-                          />
-                        </div>
-                      </div>
+                  <div key={idx} className="relative w-full h-full">
+                    <div className="w-[1440px] h-[650px]">
+                      <Image
+                          src={item}
+                          layout="fill"
+                          alt="main_image"
+                          // width={1000}
+                          // height={1000}
+                          // className="absolute w-full h-full" 
+                        />
                     </div>
                   </div>
                 );
