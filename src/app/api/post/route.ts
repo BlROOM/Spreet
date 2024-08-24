1f998dba99c51f70fe0e430ab0d9e62ee4f5b1db
BIROOM <zoon1031@naver.com>
2024-08-24 13:11:19 +0900

    prisma 설치 및 supabase 연결

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  // const res = await prisma.post.create({
+  //   data: {
+  //     content: "포스트 내용 입력!",
+  //   },
+  const res = await prisma.post.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      content: "포스트 내용 수정~",
+    },
+  });
+  return Response.json("ok!");
+}
