a68cef6bb1d287b31c92930e84710f7bca73d7c1
BIROOM <zoon1031@naver.com>
2024-09-10 16:26:47 +0900

    not-found 페이지 작성

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+import Button from "@/components/shared/Button";
+export default async function NotFound() {
+  return (
+    <main className="w-full min-h-dvh flex justify-center items-center bg-grayscale-900">
+      <div className="flex-col justify-between m-auto text-center text-grayscale-0 text-xl">
+        <p className="mb-10">
+          죄송합니다. <br />
+          해당 페이지를 찾을 수 없습니다.
+        </p>
+        <Link href="/">
+          <Button className="w-96 h-12 bg-redpoint-800  rounded-lg">
+            홈으로
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
