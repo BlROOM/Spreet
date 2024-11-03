60f4b97a17c447f9cf215eb433b096cfdd530042
BIROOM <zoon1031@naver.com>
2024-11-04 00:39:49 +0900

    Calendar 옵션 설정 및 스타일 수정
    - 년, 월. 일 글자 표시 제거
    - 외부 div박스 flex-1 적용하여 width 최대 설정

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -35,29 +35,32 @@ export default function Calendar() {
   };
 
   return (
-    <FullCalendar
-      //   editable={true} // 드래그 앤 드롭 기능 허용
-      //   events={events}
-      //   eventDrop={handleEventDrop} // 이벤트 드롭 시 동작
-      //   eventClick={handleEventClick} // 이벤트 클릭 시 수정 기능 추가
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      locale="ko" // 요일을 한글로 표시
-      selectable={true} // 날짜 선택 가능
-      //   select={handleDateSelect} // 날짜 선택 시 동작
-      headerToolbar={{
-        start: "", // 왼쪽 버튼 설정
-        center: "title", // 제목 위치
-        end: "prev,next today", // 오른쪽 버튼 설정
-      }}
-      titleFormat={{ year: "numeric", month: "long" }} // 제목 형식 설정: "2024년 10월" 형식
-      dayHeaderContent={(args) => {
-        const date = new Date(args.date);
-        const day = date.getDate(); // 날짜만 가져옴
-        return `${day}`; // "일"을 제외하고 숫자만 표시
-      }}
-      dayHeaders={true} // 요일 헤더를 표시
-      dayHeaderFormat={{ weekday: "narrow" }} // 요일을 '일', '월' 형식으로 표시
-    />
+    <div className="flex-1">
+      {/* 전체 너비 설정 */}
+      <FullCalendar
+        //   editable={true} // 드래그 앤 드롭 기능 허용
+        //   events={events}
+        //   eventDrop={handleEventDrop} // 이벤트 드롭 시 동작
+        //   eventClick={handleEventClick} // 이벤트 클릭 시 수정 기능 추가
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        locale="ko" // 요일을 한글로 표시
+        selectable={true} // 날짜 선택 가능
+        //  select={handleDateSelect} // 날짜 선택 시 동작
+        dayCellContent={(info) => {
+          // 날짜 문자열에서 '일'을 제거
+          return info.dayNumberText.replace("일", "");
+        }}
+        headerToolbar={{
+          start: "prev,next", // 이전/다음 버튼
+          center: "title", // 제목
+          end: "", // today 버튼 제거
+        }}
+        titleFormat={{ year: "numeric", month: "2-digit" }} // '2024년 05월'에서 '년'과 '월' 제거
+        dayHeaders={true} // 요일 헤더를 표시
+        dayHeaderFormat={{ weekday: "short" }} // 요일을 "일", "월" 등으로 축약
+        firstDay={0}
+      />
+    </div>
   );
 }
