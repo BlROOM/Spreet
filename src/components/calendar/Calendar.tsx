a884f2cd0607e78e2427e30b2b0fb84b929523df
BIROOM <zoon1031@naver.com>
2024-11-03 23:39:24 +0900

    FullCalendar 옵션설정

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -36,14 +36,28 @@ export default function Calendar() {
 
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]} // interaction 플러그인 추가
+      //   editable={true} // 드래그 앤 드롭 기능 허용
+      //   events={events}
+      //   eventDrop={handleEventDrop} // 이벤트 드롭 시 동작
+      //   eventClick={handleEventClick} // 이벤트 클릭 시 수정 기능 추가
+      plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      editable={true} // 드래그 앤 드롭 기능 허용
+      locale="ko" // 요일을 한글로 표시
       selectable={true} // 날짜 선택 가능
-      select={handleDateSelect} // 날짜 선택 시 동작
-      events={events}
-      eventDrop={handleEventDrop} // 이벤트 드롭 시 동작
-      eventClick={handleEventClick} // 이벤트 클릭 시 수정 기능 추가
+      //   select={handleDateSelect} // 날짜 선택 시 동작
+      headerToolbar={{
+        start: "", // 왼쪽 버튼 설정
+        center: "title", // 제목 위치
+        end: "prev,next today", // 오른쪽 버튼 설정
+      }}
+      titleFormat={{ year: "numeric", month: "long" }} // 제목 형식 설정: "2024년 10월" 형식
+      dayHeaderContent={(args) => {
+        const date = new Date(args.date);
+        const day = date.getDate(); // 날짜만 가져옴
+        return `${day}`; // "일"을 제외하고 숫자만 표시
+      }}
+      dayHeaders={true} // 요일 헤더를 표시
+      dayHeaderFormat={{ weekday: "narrow" }} // 요일을 '일', '월' 형식으로 표시
     />
   );
 }
